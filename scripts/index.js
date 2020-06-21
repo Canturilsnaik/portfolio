f82feacce0cfbcd97f05a6791521d0e346d39f26
Canturilsnaik <italo.cantur@gmail.com>
2020-06-21 10:33:14 -0300

    Colocando o modal

---
+++ scripts/index.js
@@ -0,0 +1,12 @@
+const buttonContact = document.querySelector(".menu .contato a");
+const modal = document.querySelector(".modal");
+const close = document.querySelector(".modal a + a + a");
+
+buttonContact.addEventListener("click", () => {
+  modal.classList.remove("hide")
+  event.preventDefault()
+})
+
+close.addEventListener("click", () => {
+  modal.classList.add("hide")
+})
